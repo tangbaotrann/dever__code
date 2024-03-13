05c2e9972b91043cf5e570aaf494ca93f4a491cc
tangbaotrann <tangbaotrann@gmail.com>
2024-03-13 22:13:56 +0700

    updated: push image to firebase

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 const images = {
   avatar: {
-    src: "/assets/images/noavatar.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fnoavatar.png?alt=media&token=9c741117-4105-452a-8ea8-78cab10a45dd",
     alt: "avatar-image",
   },
   bkAvatar: {
@@ -11,47 +11,47 @@ const images = {
 
 export const icons = {
   menuMobile: {
-    src: "/assets/images/burger-bar.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fburger-bar.png?alt=media&token=76ca8d57-2666-46d9-b14d-a48309999484",
     alt: "menu-mobile-icon",
   },
   closeMenuMobile: {
-    src: "/assets/images/cross-mark.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fcross-mark.png?alt=media&token=0d586312-8b0e-4479-933c-8f51f93ffa3c",
     alt: "close-menu-mobile-icon",
   },
   loading: {
-    src: "/assets/images/loading.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Floading.png?alt=media&token=8146d4ef-b8c6-48e4-87af-fd6dfab90ce0",
     alt: "loading-icon",
   },
   github: {
-    src: "/assets/images/github.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fgithub.png?alt=media&token=920da180-dc64-4ce8-a0b5-6b649e66eda5",
     alt: "github-icon",
   },
   google: {
-    src: "/assets/images/google.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fgoogle.png?alt=media&token=eff6f434-82c6-49a6-8506-4617fc5485e4",
     alt: "google-icon",
   },
   logout: {
-    src: "/assets/images/logout.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Flogout.png?alt=media&token=860914ed-e286-4dc1-b2ec-40dfeace2b8f",
     alt: "logout-icon",
   },
   notFound: {
-    src: "/assets/images/error-404.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Ferror-404.png?alt=media&token=ba9b87e7-699b-4d69-87f9-cf1812e1f1d5",
     alt: "not-found-icon",
   },
   arrowGoBack: {
-    src: "/assets/images/arrow.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Farrow.png?alt=media&token=2cd32f1c-d746-4a41-bee1-1878b0dd1b8f",
     alt: "arrow-go-back-icon",
   },
   error: {
-    src: "/assets/images/error.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Ferror.png?alt=media&token=b9bc277d-1902-49e8-8e9a-d28999fe3698",
     alt: "error-icon",
   },
   blog: {
-    src: "/assets/images/blog.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fblog.png?alt=media&token=4b896156-e1f9-410d-96c3-8647501237a3",
     alt: "blog-icon",
   },
   messenger: {
-    src: "/assets/images/messenger.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fmessenger.png?alt=media&token=0b9b9181-5908-464f-9088-7f7312b045a5",
     alt: "messenger-icon",
   },
   QandA: {
@@ -82,46 +82,46 @@ export const icons = {
 
 export const slidesTop = [
   {
-    src: "/assets/images/html-5.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fhtml-5.png?alt=media&token=3a4cf00b-df44-44fc-83a4-cf6f815042e0",
     alt: "html-icon",
   },
   {
-    src: "/assets/images/css-3.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fcss-3.png?alt=media&token=03a13563-2330-430a-97b2-b81a4607ada5",
     alt: "css-icon",
   },
   {
-    src: "/assets/images/js.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fjs.png?alt=media&token=a7d6a58b-1d01-4ae9-b2af-4e421a5a79c3",
     alt: "js-icon",
   },
   {
-    src: "/assets/images/react.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Freact.png?alt=media&token=668e52be-ff48-41b6-9676-c33c9fe0f035",
     alt: "react-icon",
   },
   {
-    src: "/assets/images/sass.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fsass.png?alt=media&token=935f1ff3-d610-44d0-91d4-54ab16cd55d4",
     alt: "sass-icon",
   },
 ];
 
 export const slidesBottom = [
   {
-    src: "/assets/images/nextjs.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fnextjs.png?alt=media&token=7643b6f9-42eb-4af8-bd5d-c24fd8aeb4dc",
     alt: "nextjs-icon",
   },
   {
-    src: "/assets/images/nodejs.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fnodejs.png?alt=media&token=46e4e163-70de-4fc4-ab13-862c67800b2a",
     alt: "nodejs-icon",
   },
   {
-    src: "/assets/images/github.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fgithub.png?alt=media&token=920da180-dc64-4ce8-a0b5-6b649e66eda5",
     alt: "github-icon",
   },
   {
-    src: "/assets/images/docker.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fdocker.png?alt=media&token=55fe379c-b121-4506-ac7a-089506e29510",
     alt: "docker-icon",
   },
   {
-    src: "/assets/images/mongoDB.png",
+    src: "https://firebasestorage.googleapis.com/v0/b/devercode-72ca6.appspot.com/o/images%2Fmongodb.png?alt=media&token=c48f16a0-d450-4022-8e48-669ae97ae3de",
     alt: "mongoDB-icon",
   },
 ];
